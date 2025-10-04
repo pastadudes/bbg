e49071f85a4f373eeb2be006a59b5471977e30b6
pastaya <contact@pastaya.net>
2025-10-04 21:14:47 +0200

    ok so the flake was broken

--- flake.nix
+++ flake.nix
@@ -1,40 +1,63 @@
+# https://github.com/cpu/rust-flake/blob/main/flake.nix
 {
-  # this had to be made because FUCK docker
-  description = "development environment for bbg";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }: let
-    pkgs = import nixpkgs {
-      overlays = [ rust-overlay.overlay ];
-    };
-  in
-  {
-    devShells.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.rustc
-        pkgs.cargo
-        pkgs.pkg-config     
-        pkgs.openssl        
-        pkgs.zlib           
-        pkgs.libpng         
-        pkgs.freetype       
-        pkgs.fontconfig     
-      ];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', pkgs, lib, system, ... }:
+        let
+          runtimeDeps = with pkgs; [ openssl ];
+          buildDeps = with pkgs; [ pkg-config rustPlatform.bindgenHook font-config ];
+          devDeps = with pkgs; [ gdb ];
 
-      RUSTFLAGS = "-C target-cpu=native";
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          msrv = cargoToml.package.rust-version;
 
-      shellHook = ''
-        echo "welcome to the bbg dev shell!"
-        echo "rust version: $(rustc --version)"
-        echo "cargo version: $(cargo --version)"
-      '';
+          rustPackage = features:
+            (pkgs.makeRustPlatform {
+              cargo = pkgs.rust-bin.stable.latest.minimal;
+              rustc = pkgs.rust-bin.stable.latest.minimal;
+            }).buildRustPackage {
+              inherit (cargoToml.package) name version;
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              buildFeatures = features;
+              buildInputs = runtimeDeps;
+              nativeBuildInputs = buildDeps;
+              # Uncomment if your cargo tests require networking or otherwise
+              # don't play nicely with the Nix build sandbox:
+              # doCheck = false;
+            };
+
+          mkDevShell = rustc:
+            pkgs.mkShell {
+              shellHook = ''
+                export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+              '';
+              buildInputs = runtimeDeps;
+              nativeBuildInputs = buildDeps ++ devDeps ++ [ rustc ];
+            };
+        in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+
+          packages.default = self'.packages.example;
+          devShells.default = self'.devShells.nightly;
+
+          packages.example = (rustPackage "foobar");
+          packages.example-base = (rustPackage "");
+
+          devShells.nightly = (mkDevShell (pkgs.rust-bin.selectLatestNightlyWith
+            (toolchain: toolchain.default)));
+          devShells.stable = (mkDevShell pkgs.rust-bin.stable.latest.default);
+          devShells.msrv = (mkDevShell pkgs.rust-bin.stable.${msrv}.default);
+        };
     };
-  };
 }
-
-# wheres the shell.nix??
-# ...
